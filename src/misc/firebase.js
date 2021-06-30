3dfbfef21990a88db1e171513678aea138a1fd87
Harsh-Manoor <harshmanoor@gmail.com>
2021-07-01 00:38:35 +0530

    firebase init

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+const config = {
+    apiKey: "AIzaSyCaDJ1AXM4n1e023vEB_GyqBgBDuvmI7II",
+    authDomain: "web-chat-app-78753.firebaseapp.com",
+    databaseURL: "https://web-chat-app-78753-default-rtdb.firebaseio.com",
+    projectId: "web-chat-app-78753",
+    storageBucket: "web-chat-app-78753.appspot.com",
+    messagingSenderId: "416621358890",
+    appId: "1:416621358890:web:b6cd3513314e137c167853"
+};
+
+const app = firebase.initializeApp(config);
